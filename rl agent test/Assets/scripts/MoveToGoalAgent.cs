8608798e3f4c9133527d64a751de534ff7d7f1d1
womyat <jannes.doege@manos-dresden.lernsax.de>
2021-05-24 14:30:28 +0200

    d

---
+++ rl agent test/Assets/scripts/MoveToGoalAgent.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoalAgent : Agent
+{
+    [SerializeField] private Transform targetTrans;
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
+    private float moveSpeed = 5f;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(4.5f, 1f), 0, Random.Range(-3.5f, 4f));
+        targetTrans.localPosition = new Vector3(Random.Range(-4f, -1f), -1f, Random.Range(-3.5f, 4f));
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTrans.localPosition);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut){
+        ActionSegment<float> continousActions = actionsOut.ContinuousActions;
+        continousActions[0] = Input.GetAxisRaw("Horizontal");
+        continousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.TryGetComponent<Goal>(out Goal goal)){
+            SetReward(+1f);
+            floorMeshRenderer.material = winMaterial;
+            EndEpisode();    
+        }
+        if (other.TryGetComponent<Wall>(out Wall wall)){
+            SetReward(-1f);
+            floorMeshRenderer.material = loseMaterial;
+            EndEpisode();    
+        }         
+    }   
+}
