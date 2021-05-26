14fd42e45d9e1da2255051e41889be688ddbb5cf
Jannes Döge <doege.jannes@gmail.com>
2021-05-26 17:50:23 +0200

    q

---
+++ rl agent test/python/buffer.py
@@ -0,0 +1,19 @@
+import collections
+import numpy as np
+
+Experience = collections.namedtuple("Experience", field_names=["state", "action", "reward", "done", "next_state"])
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.buffer = collections.deque(maxlen=capacity)
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def add_exp(self, exp: Experience):
+        self.buffer.append(exp)
+
+    def sample(self, batch_size):
+        idxs = np.random.choice(len(self.buffer), batch_size, replace=False)
+        states, actions, rewards,  dones, next_states = zip(*[self.buffer[idx] for idx in idxs])
+        return np.array(states), np.array(actions),np.array(rewards, dtype=np.float32), np.array(dones, dtype=np.uint8), np.array(next_states)
---
+++ rl agent test/python/model.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+#simple linear deep q network
+class DQN(nn.Module):
+    def __init__(self, input_space, action_space, hidden_units_one=128, hidden_units_two=128):
+        super(DQN, self).__init__()
+        self.input_shape = input_space
+        self.action_space = action_space
+        self.hidden_units_one = hidden_units_one
+        self.hidden_units_two = hidden_units_two
+
+        self.net = nn.Sequential(
+            nn.Linear(self.input_shape, self.hidden_units_two),
+            nn.ReLU(),
+            nn.Linear(self.hidden_units_one, self.hidden_units_two),
+            nn.ReLU(),
+            nn.Linear(self.hidden_units_two, self.action_space),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
